e729be5e9025b62177f8743ef29154ce3a688de5
Rajib Ahmed <43868221+spades1404@users.noreply.github.com>
2020-07-15 10:37:00 +0100

    Update otocomparison.py
    Removed unnecessary line

--- ABMC19/Agents/Actions/Comparisons/otocomparison.py
+++ ABMC19/Agents/Actions/Comparisons/otocomparison.py
@@ -4,7 +4,7 @@ def onetoonecomparison(primaryAgent,secondaryAgent):
     if secondaryAgent.infected == False or secondaryAgent.pos == secondaryAgent.homeCoords: #if they are not infected or in their home
         return
 
-    probability = 0.5 #going to say its a coinflip wheter they are infected or not, can be changed
+    #going to say its a coinflip wheter they are infected or not, can be changed
     outcome = random.uniform(0,1)
 
     if outcome > 0.5 and primaryAgent.immune == False:
